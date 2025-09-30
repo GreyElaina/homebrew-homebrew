5ae684499430a6de8d759cfeaf058fc8c3f52482
GreyElaina <GreyElaina@outlook.com>
2025-09-30 21:03:50 +0800

    stagenow: update to latest

--- Formula/stagenow.rb
+++ Formula/stagenow.rb
@@ -1,9 +1,9 @@
 class Stagenow < Formula
   desc "CLI and daemon to automate Stage Manager per space"
   homepage "https://github.com/GreyElaina/StageNow"
-  url "https://github.com/GreyElaina/StageNow/archive/69416bbe92e118d18c6db264cb5b22ca23336c6c.tar.gz"
-  version "0.0.0.20250929"
-  sha256 "75b6722f6be1473a6039e864ee059a62df2d25d69c79e308681de4bffec5d7cb"
+  url "https://github.com/GreyElaina/StageNow/archive/364ed7464b0acf660bb0b135928d00f8e9d759d0.tar.gz"
+  version "0.0.0.20250930"
+  sha256 "a6913e7002e19d72e6beec2d1fe26da88da28dbf3cff892c137370584d8422a3"
   head "https://github.com/GreyElaina/StageNow.git", branch: "master"
 
   depends_on xcode: ["14.0", :build]
@@ -14,46 +14,6 @@ class Stagenow < Formula
     bin.install ".build/release/StageNow"
     pkgshare.install "Resources/config.json"
     pkgshare.install "Resources/Raycast"
-
-    # Write a custom launchd plist with MachServices for users to start via `brew services --file`
-    (pkgshare/"stagenow.mach.plist").write <<~PLIST
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>by.akashina.stagenow</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/StageNow</string>
-            <string>--daemon</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <dict>
-            <key>SuccessfulExit</key>
-            <false/>
-          </dict>
-          <key>MachServices</key>
-          <dict>
-            <key>by.akashina.stagenow</key>
-            <true/>
-          </dict>
-          <key>StandardOutPath</key>
-          <string>#{var}/log/stagenow/StageManager-daemon.log</string>
-          <key>StandardErrorPath</key>
-          <string>#{var}/log/stagenow/StageManager-daemon.err.log</string>
-          <key>EnvironmentVariables</key>
-          <dict>
-            <key>PATH</key>
-            <string>/opt/homebrew/bin:/opt/homebrew/sbin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-          </dict>
-          <key>ProcessType</key>
-          <string>Background</string>
-        </dict>
-      </plist>
-    PLIST
   end
 
   def post_install
